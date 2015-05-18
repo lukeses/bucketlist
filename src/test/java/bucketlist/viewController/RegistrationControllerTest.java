eb3a97df01b2db2ef01aec54e8c7530ccd57c0dd
Danielek98 <danielek2606@gmail.com>
2015-05-19 00:19:16 +0200

    RegistrationController tests added

--- src/test/java/bucketlist/viewController/RegistrationControllerTest.java
+++ src/test/java/bucketlist/viewController/RegistrationControllerTest.java
@@ -12,6 +12,7 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import static org.junit.Assert.*;
+import static org.easymock.EasyMock.*;
 
 /**
  *
@@ -54,192 +55,86 @@ public class RegistrationControllerTest {
     }
 
     /**
-     * Test of getFirstName method, of class RegistrationController.
-     */
-    @Test
-    public void testGetFirstName() {
-        System.out.println("getFirstName");
-        RegistrationController instance = new RegistrationController();
-        String expResult = "";
-        String result = instance.getFirstName();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of setFirstName method, of class RegistrationController.
-     */
-    @Test
-    public void testSetFirstName() {
-        System.out.println("setFirstName");
-        String firstName = "";
-        RegistrationController instance = new RegistrationController();
-        instance.setFirstName(firstName);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getLastName method, of class RegistrationController.
-     */
-    @Test
-    public void testGetLastName() {
-        System.out.println("getLastName");
-        RegistrationController instance = new RegistrationController();
-        String expResult = "";
-        String result = instance.getLastName();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of setLastName method, of class RegistrationController.
-     */
-    @Test
-    public void testSetLastName() {
-        System.out.println("setLastName");
-        String lastName = "";
-        RegistrationController instance = new RegistrationController();
-        instance.setLastName(lastName);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getPassword1 method, of class RegistrationController.
-     */
-    @Test
-    public void testGetPassword1() {
-        System.out.println("getPassword1");
-        RegistrationController instance = new RegistrationController();
-        String expResult = "";
-        String result = instance.getPassword1();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of setPassword1 method, of class RegistrationController.
-     */
-    @Test
-    public void testSetPassword1() {
-        System.out.println("setPassword1");
-        String password1 = "";
-        RegistrationController instance = new RegistrationController();
-        instance.setPassword1(password1);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getPassword2 method, of class RegistrationController.
-     */
-    @Test
-    public void testGetPassword2() {
-        System.out.println("getPassword2");
-        RegistrationController instance = new RegistrationController();
-        String expResult = "";
-        String result = instance.getPassword2();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of setPassword2 method, of class RegistrationController.
-     */
-    @Test
-    public void testSetPassword2() {
-        System.out.println("setPassword2");
-        String password2 = "";
-        RegistrationController instance = new RegistrationController();
-        instance.setPassword2(password2);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getEmail1 method, of class RegistrationController.
-     */
-    @Test
-    public void testGetEmail1() {
-        System.out.println("getEmail1");
-        RegistrationController instance = new RegistrationController();
-        String expResult = "";
-        String result = instance.getEmail1();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of setEmail1 method, of class RegistrationController.
-     */
-    @Test
-    public void testSetEmail1() {
-        System.out.println("setEmail1");
-        String email1 = "";
-        RegistrationController instance = new RegistrationController();
-        instance.setEmail1(email1);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of getEmail2 method, of class RegistrationController.
-     */
-    @Test
-    public void testGetEmail2() {
-        System.out.println("getEmail2");
-        RegistrationController instance = new RegistrationController();
-        String expResult = "";
-        String result = instance.getEmail2();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of setEmail2 method, of class RegistrationController.
-     */
-    @Test
-    public void testSetEmail2() {
-        System.out.println("setEmail2");
-        String email2 = "";
-        RegistrationController instance = new RegistrationController();
-        instance.setEmail2(email2);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of setDatabase method, of class RegistrationController.
-     */
-    @Test
-    public void testSetDatabase() {
-        System.out.println("setDatabase");
-        BucketlistController database = null;
-        RegistrationController instance = new RegistrationController();
-        instance.setDatabase(database);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
      * Test of registerNewUser method, of class RegistrationController.
      */
     @Test
     public void testRegisterNewUser() {
-        System.out.println("registerNewUser");
-        RegistrationController instance = new RegistrationController();
-        String expResult = "";
-        String result = instance.registerNewUser();
-        assertEquals(expResult, result);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
+        String newUser = "example@example.com";
+        String newPassword = "pass1";
+        String firstName = "Ela";
+        String lastName = "Kowalska";
+        BucketlistController controllerMock = createMock(BucketlistController.class);
+        controllerMock.openSession();
+        expect(controllerMock.userExists(newUser)).andReturn(false);
+        controllerMock.closeSession();
+        controllerMock.openSession();
+        controllerMock.addNewUser(firstName, lastName, newUser, newPassword);
+        controllerMock.closeSession();
+        
+        replay(controllerMock);
+        RegistrationController registrationController = new RegistrationController();
+        registrationController.setDatabase(controllerMock);
+        registrationController.setFirstName(firstName);
+        registrationController.setLastName(lastName);
+        registrationController.setEmail1(newUser);
+        registrationController.setEmail2(newUser);
+        registrationController.setPassword1(newPassword);
+        registrationController.setPassword2(newPassword);
+        registrationController.registerNewUser();
+        
+        verify(controllerMock);
+        }
     
+    /**
+     * Test of registerNewUser method, of class RegistrationController.
+     */
+    @Test
+    public void testRegisterNewUserWithExistingMail() {
+        String newUser = "example@example.com";
+        String newPassword = "pass1";
+        String firstName = "Ela";
+        String lastName = "Kowalska";
+        BucketlistController controllerMock = createMock(BucketlistController.class);
+        controllerMock.openSession();
+        expect(controllerMock.userExists(newUser)).andReturn(true);
+        controllerMock.closeSession();
+        
+        replay(controllerMock);
+        RegistrationController registrationController = new RegistrationController();
+        registrationController.setDatabase(controllerMock);
+        registrationController.setFirstName(firstName);
+        registrationController.setLastName(lastName);
+        registrationController.setEmail1(newUser);
+        registrationController.setEmail2(newUser);
+        registrationController.setPassword1(newPassword);
+        registrationController.setPassword2(newPassword);
+        registrationController.registerNewUser();
+        
+        verify(controllerMock);
+        }
+    
+    /**
+     * Test of registerNewUser method, of class RegistrationController.
+     */
+    @Test
+    public void testRegisterNewUserWithIncorrectPassword() {
+        String newUser = "example@example.com";
+        String newPassword = "pass1";
+        String firstName = "Ela";
+        String lastName = "Kowalska";
+        BucketlistController controllerMock = createMock(BucketlistController.class);
+        
+        replay(controllerMock);
+        RegistrationController registrationController = new RegistrationController();
+        registrationController.setDatabase(controllerMock);
+        registrationController.setFirstName(firstName);
+        registrationController.setLastName(lastName);
+        registrationController.setEmail1(newUser);
+        registrationController.setEmail2(newUser);
+        registrationController.setPassword1(newPassword);
+        registrationController.setPassword2(newPassword + "1");
+        registrationController.registerNewUser();
+        
+        verify(controllerMock);
+        }
 }
