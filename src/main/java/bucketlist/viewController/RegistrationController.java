83f18a3f73721497481a982dd8e16d6d2b1f36cc
Danielek98 <danielek2606@gmail.com>
2015-05-18 18:14:50 +0200

    Registration page added.

---
+++ src/main/java/bucketlist/viewController/RegistrationController.java
@@ -0,0 +1,82 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bucketlist.viewController;
+
+import bucketlist.controller.BucketlistController;
+import javax.faces.application.FacesMessage;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+
+/**
+ *
+ * @author Daniel
+ */
+@ManagedBean
+@SessionScoped
+public class LoginController {
+
+    @ManagedProperty(value = "#{databaseDAO}")
+    private BucketlistController database;
+    private String userName;
+    private String userPassword;
+
+    /**
+     * @param database the database to set
+     */
+    public void setDatabase(BucketlistController database) {
+        this.database = database;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    private boolean loggedIn;
+
+    public String tryToLogin() {
+        database.openSession();
+        boolean result = database.checkPassword(userName, userPassword);
+        database.CloseSession();
+
+        if (result) {
+            loggedIn = true;
+            return "/secured/userItems.xhtml?faces-redirect=true";
+
+        }
+        FacesMessage msg = new FacesMessage("Login error!", "ERROR MSG");
+        msg.setSeverity(FacesMessage.SEVERITY_ERROR);
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+        return null;
+    }
+
+    public String logout() {
+        loggedIn = false;
+
+        FacesMessage msg = new FacesMessage("Logout success!", "INFO MSG");
+        msg.setSeverity(FacesMessage.SEVERITY_INFO);
+        FacesContext.getCurrentInstance().addMessage(null, msg);
+
+        return "/logpanel.xhtml?faces-redirect=true";
+    }
+
+    public boolean isLoggedIn() {
+        return loggedIn;
+    }
+}
