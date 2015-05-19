1133b96bc47fd15530964421a76da19716622290
blaballong <blaballong@gmail.com>
2015-05-19 02:09:15 +0200

    !

--- src/test/java/bucketlist/viewController/LoginFilterTest.java
+++ src/test/java/bucketlist/viewController/LoginFilterTest.java
@@ -21,81 +21,12 @@ import static org.junit.Assert.*;
  * @author Adam
  */
 public class LoginFilterTest {
-    
-    /**
-     *
-     */
+
     public LoginFilterTest() {
     }
-    
-    /**
-     *
-     */
-    @BeforeClass
-    public static void setUpClass() {
-    }
-    
-    /**
-     *
-     */
-    @AfterClass
-    public static void tearDownClass() {
-    }
-    
-    /**
-     *
-     */
-    @Before
-    public void setUp() {
-    }
-    
-    /**
-     *
-     */
-    @After
-    public void tearDown() {
-    }
-
-    /**
-     * Test of init method, of class LoginFilter.
-     * @throws java.lang.Exception
-     */
-    @Test
-    public void testInit() throws Exception {
-        System.out.println("init");
-        FilterConfig fc = null;
-        LoginFilter instance = new LoginFilter();
-        instance.init(fc);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of doFilter method, of class LoginFilter.
-     * @throws java.lang.Exception
-     */
-    @Test
-    public void testDoFilter() throws Exception {
-        System.out.println("doFilter");
-        ServletRequest request = null;
-        ServletResponse response = null;
-        FilterChain chain = null;
-        LoginFilter instance = new LoginFilter();
-        instance.doFilter(request, response, chain);
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
-    }
-
-    /**
-     * Test of destroy method, of class LoginFilter.
-     */
+ 
     @Test
     public void testDestroy() {
-        System.out.println("destroy");
-        LoginFilter instance = new LoginFilter();
-        instance.destroy();
-        // TODO review the generated test code and remove the default call to fail.
-        fail("The test case is a prototype.");
     }
     
 }
